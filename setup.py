6b1030cddb19f7f4d4896b9eb2fd91616e74756c
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-02-10 14:35:35 +0100

    Add the Python3 classifiers.

--- setup.py
+++ setup.py
@@ -24,25 +24,31 @@ with io.open(os.path.join(here, 'README.rst'), encoding='utf8') as f:
 with io.open(os.path.join(here, 'CHANGELOG.md'), encoding='utf8') as f:
     CHANGES = f.read()
 
-setup(name="pywebpush",
-      version=__version__,
-      packages=find_packages(),
-      description='WebPush publication library',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=["Topic :: Internet :: WWW/HTTP",
-                   "Programming Language :: Python :: Implementation :: PyPy",
-                   'Programming Language :: Python',
-                   "Programming Language :: Python :: 2",
-                   "Programming Language :: Python :: 2.7"
-                   ],
-      keywords='push webpush publication',
-      author="jr conlin",
-      author_email="src+webpusher@jrconlin.com",
-      url='https://github.com/web-push-libs/pywebpush',
-      license="MPL2",
-      test_suite="nose.collector",
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=read_from('requirements.txt'),
-      tests_require=read_from('test-requirements.txt')
-      )
+setup(
+    name="pywebpush",
+    version=__version__,
+    packages=find_packages(),
+    description='WebPush publication library',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords='push webpush publication',
+    author="JR Conlin",
+    author_email="src+webpusher@jrconlin.com",
+    url='https://github.com/web-push-libs/pywebpush',
+    license="MPL2",
+    test_suite="nose.collector",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=read_from('requirements.txt'),
+    tests_require=read_from('test-requirements.txt')
+)
